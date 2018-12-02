4a86bd470835e3dfaf117954fb82507de0dd8c1e
dream254111 <dream254111@hotmail.com>
2018-12-02 12:29:49 +0700

    Dream

--- ArticleSearcher.java
+++ ArticleSearcher.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import java.util.Collections;
 import java.util.HashMap;
@@ -13,19 +12,20 @@ import java.util.stream.Stream;
 import opennlp.tools.stemmer.PorterStemmer;
 
 public class ArticleSearcher {
-	//set of document
+	
+	// Set of document
 	List<Document> docs = null;
 	public static PorterStemmer porterStemmer = new PorterStemmer();
 	public static final Set<String> stopWords = Stream.of("a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves").collect(Collectors.toSet());
-	private HashMap<String, HashSet<String>> newDocument; //Remove duplicate of token in the document
-	private HashMap<String, Document> docWID; //Map docID with doc
-	private HashSet<String> termPool; //Keep all tokens of all docs
-	private HashMap<String, Double> idf; //Map termName with value of idf
-	private HashMap<String, Double> rank; //Map score with docID
-	private HashMap<String, Double> docMagnitude; //Map docID with their magnitude
+	private HashMap<String, HashSet<String>> newDocument;	//Remove duplicate of token in the document
+	private HashMap<String, Document> docWID;				//Map docID with doc
+	private HashSet<String> termPool;						//Keep all tokens of all docs
+	private HashMap<String, Double> idf;					//Map termName with value of idf
+	private HashMap<String, Double> rank;					//Map score with docID
+	private HashMap<String, Double> docMagnitude;			//Map docID with their magnitude
 	private HashMap<String, Integer> debug;
 	
-	public ArticleSearcher(File file) {
+	 ArticleSearcher(File file) throws IOException {
 		this.docs = parseDocumentFromFile(file);
 		newDocument = new HashMap<String, HashSet<String>>();
 		termPool = new HashSet<String>();
@@ -36,17 +36,17 @@ public class ArticleSearcher {
 		debug = new HashMap<String, Integer>();
 		docWID = new HashMap<String, Document>();
 		
-		//Remove duplicate of document, and Map it in newDocument, termPool is finish
+		// Remove duplicate of document, and Map it in newDocument, termPool is finish
 		for (Document doc : this.docs) {
 			HashSet<String> tempSet = new HashSet<String>();
 			tempSet.addAll(doc.getTokens());
-			termPool.addAll(doc.getTokens()); //Add token into TermPool
+			termPool.addAll(doc.getTokens());	//Add token into TermPool
 			newDocument.put(doc.getA_id(), tempSet);
 			
 			docWID.put(doc.getA_id(), doc);
 		}
 		
-		//Calculate idf of each Token (Less Accuracy, but spend less time)
+		// Calculate idf of each Token (Less Accuracy, but spend less time)
 		for (Map.Entry<String, HashSet<String>> entry : newDocument.entrySet()) {
 			for (String str : entry.getValue()) {
 				if(!debug.keySet().contains(str))
@@ -83,22 +83,16 @@ public class ArticleSearcher {
 		}
 	}
 	
-	public static List<Document> parseDocumentFromFile(File file)
-	{
+	public static List<Document> parseDocumentFromFile(File file){
 		int i = 0;
 		List<Document> documents = new Vector<Document>();
-		for(File fileEntry : file.listFiles())
-		{
+		for(File fileEntry : file.listFiles()) {
 			//add a document entry to documents
 			Document doc = new Document(fileEntry);
 			List<String> tokens = tokenize(doc.getContent());
 			doc.setTokens(tokens);
 			documents.add(doc);
-			System.out.println((i++));
 		}
-		
-				
-//		System.out.println("@@@ Finished loading "+docs.size()+" docs from "+file.getName());
 				
 		return documents;
 	}
@@ -121,7 +115,7 @@ public class ArticleSearcher {
 			if(t.length() <= 1) continue;
 			if(stopWords.contains(t)) continue;
 			 
-			t = porterStemmer.stem(t);
+//			t = porterStemmer.stem(t);
 			tokens.add(t);
 		}
 		//return
@@ -182,13 +176,13 @@ public class ArticleSearcher {
 			
 		}
 		
-		//Find the first k-doc
-		int[] kDoc = new int[k];
+		// Find the first k-doc
+		String[] kDoc = new String[k];
 		int pointer = 0;
 		
 		while(pointer != k) {
 			double max = Integer.MIN_VALUE;
-			int doc = -1;
+			String doc = "";
 			
 			for (Map.Entry<String, Double> entry : rank.entrySet()) {
 				boolean check = true;
@@ -222,5 +216,14 @@ public class ArticleSearcher {
 		
 		return ret;
 		/***********************************************/
+	}
+	
+	public static void displaySearchResults(List<SearchResult> results)
+	{	StringBuilder str = new StringBuilder();
+		for(int i = 0; i < results.size(); i++)
+		{
+			str.append("<"+(i+1)+">"+results.get(i));
+		}
+		System.out.println(str);
 	}
 }
--- Dream (GUI)/Runner.java
+++ Dream (GUI)/Runner.java
@@ -1,11 +1,20 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class Runner {
 	
-	public static void main(String[] args) {
-		Model model = new Model();
-		View view = new View();
+	public static void main(String[] args) throws IOException {
+		File file = new File("./DB");
+		ArticleSearcher test = new ArticleSearcher(file);
 		
-		Controller controller = new Controller(view, model);
+		List<SearchResult> result = test.search("BUILDING", 10);
+		test.displaySearchResults(result);
+		
+//		Model model = new Model();
+//		View view = new View();
+//		
+//		Controller controller = new Controller(view, model);
 	}
 	
 }
